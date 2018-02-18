9534ae4f43d05ef0e26cf4e75cdd4781573643ee
Elizabeth Wong <lizzwong@gmail.com>
2018-02-17 18:05:59 -0600

    basemode done

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,8 +5,8 @@ function onReady() {
     getAllTasks();
     $('#submitButton').on('click',function(event){
         event.preventDefault();
-        emptyInputs();
         addTask();
+        emptyInputs();
     })//submit button
 
     $('.table').on('click', '.deleteButton', function () {
@@ -85,9 +85,15 @@ $.ajax({
 function displayTasks(bananas){
     $('#listTasks').empty();
     for( let banana of bananas ){
+        if (banana.status == 'Incomplete'){
         $('#listTasks').append(`<tr><td>${banana.dateadded.substring(0, 10)}</td><td>${banana.task}</td>
         <td>${banana.status}</td><td><button class="completeButton" data-id=${banana.id}>Complete</button></td>
+        <td><button class="deleteButton" data-id=${banana.id}>Delete</button></td></tr>`)}
+        else {
+            $('#listTasks').append(`<tr><td class="completeTask">${banana.dateadded.substring(0, 10)}</td>
+        <td class="completeTask">${banana.task}</td><td>${banana.status}</td><td></td>
         <td><button class="deleteButton" data-id=${banana.id}>Delete</button></td></tr>`)
+        }
     }
 }//end display tasks function
 
@@ -101,6 +107,6 @@ function getNewTask(){
 }//end getting inputs from DOM
 
 function emptyInputs(){
-    $('.date').val(),
-    $('taskToDo').val()
+    $('.date').val(''),
+    $('taskToDo').val('')
 }
